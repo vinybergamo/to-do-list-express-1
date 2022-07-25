f7bfc271e8db5507adccf2812ce25c7c18aef4ba
Guilherme Castro <guii_1@hotmail.com>
2022-07-25 19:42:48 -0300

    criação das rotas

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 const express = require('express')
+const checklistRouter = require('./src/routes/checklist')
+
+
+const app = express()
+app.use(express.json())
+
+app.use("/checklist",checklistRouter)
+
+app.listen(3000,() => {
+    console.log('servidor ativo!')
+})
---
+++ src/routes/checklist.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const router = express.Router()
+
+
+router.get('/', (req, res) => {
+    console.log('Ola')
+    res.send()
+})
+
+router.post('/', (req, res) => {
+    console.log(req.body)
+    res.status(200).send(req.body)
+})
+
+router.get('/:id', (req, res) => {
+    console.log(req.params.id)
+    res.send(`ID: ${req.params.id}`)
+})
+
+router.put('/:id', (req, res) => {
+    console.log(req.params.id)
+    res.send(`ID: ${req.params.id}`)
+})
+
+router.delete('/:id', (req, res) => {
+    console.log(req.params.id)
+    res.send(`ID: ${req.params.id}`)
+})
+
+module.exports = router
