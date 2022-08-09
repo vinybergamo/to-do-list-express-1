b10753b09b45e4395b2e40f3588030cedf991628
Guilherme Castro <guii_1@hotmail.com>
2022-08-09 12:11:14 -0300

    adicionando readme e explicando projeto

---
+++ README.md
@@ -0,0 +1,16 @@
+<h1>Bem vindo ao meu Aplicativo de criação de notas e tarefas, feito com NODE.js.</h1>
+
+<section>
+    <h2>Objetivo:</h2>
+        <p>Esse projeto tem como objetivo aplicar os conhecimnentos adquiridos nas aulas de node.js da OnebitCode.</p>
+</section>
+
+<section>
+    <h2>Tecnologias Utilizadas:</h2>
+        <ul>
+            <li>Node.js</li>
+            <li>Express</li>
+            <li>MongoDB</li>
+            <li>ejs</li>
+        </ul>
+</section>   
--- src/routes/task.js
+++ src/routes/task.js
@@ -43,6 +43,19 @@ checklistDependentRoute.post('/:id/tasks', async(req, res) => {
     }
 })
 
+simpleRouter.put('/:id', async(req, res) => {
+    let task = await Task.findById(req.params.id)
+    console.log(task)
+    try {   
+        task.set(req.body.task)
+        await task.save()
+        res.status(200).json({ task })
+    } catch (error) {
+        let errors = error.errors
+        res.status(422).json({ task: {...errors }}) 
+    }
+})
+
 module.exports = {
      checklistDependent: checklistDependentRoute,
      simple: simpleRouter
